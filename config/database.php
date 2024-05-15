7b7e01967e072c6c6904f9aaea209b8c90e1ed1e
kenny <kenny@qordev.com>
2024-05-15 16:16:01 -0600

    Añadiendo conf, DB

--- config/database.php
+++ config/database.php
@@ -57,7 +57,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'strict' => true,
-            'engine' => null,
+            'engine' => 'InnoDB',
             'options' => extension_loaded('pdo_mysql') ? array_filter([
                 PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
                 PDO::MYSQL_ATTR_MAX_BUFFER_SIZE => 16777216,
